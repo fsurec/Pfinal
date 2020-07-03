79d4332e2a12c7848bbbfd8a726fbf7510ae0c36
mfscostop@gmail.com <mfscostop@gmail.com>
2020-07-02 19:37:37 -0600

    Ingreso de Clientes

--- Clientes.aspx.cs
+++ Clientes.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Management;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,7 +11,7 @@ namespace Pfinal
 {
     public partial class Clientes : System.Web.UI.Page
     {
-        List<Clientes> cliente = new List<Clientes>();
+        List<ClientesC> cliente = new List<ClientesC>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,9 +20,37 @@ namespace Pfinal
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Clientes temp = new Clientes();
-            
-            
+            ClientesC temp = new ClientesC();
+            temp.Nit = txtdpi.Text;
+            temp.Apellido = txtapellido.Text;
+            temp.Nombre = txtnombre.Text;
+            temp.Telefono = txtcel.Text;
+            temp.Dirección = txtdireccion.Text;
+
+            cliente.Add(temp);
+
+            var archivo =Server.MapPath("~/Clientes.txt");
+
+            FileStream stream = new FileStream(archivo, FileMode.Open, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            foreach(var p in cliente)
+            {
+                writer.WriteLine(p.Nit);
+                writer.WriteLine(p.Apellido);
+                writer.WriteLine(p.Nombre);
+                writer.WriteLine(p.Telefono);
+                writer.WriteLine(p.Dirección);
+            }
+            writer.Close();
+            txtdpi.Text = null;
+            txtapellido.Text = null;
+            txtnombre.Text = null;
+            txtcel.Text = null;
+            txtdireccion.Text = null;
+             
         }
+
+        
     }
 }
